dfff2de7566e5caab8fa748a1fef216c979f28aa
tisuela <ntisuela@uci.edu>
2021-11-17 18:19:24 -0800

    add discord link

--- packages/website/src/pages/index.tsx
+++ packages/website/src/pages/index.tsx
@@ -25,7 +25,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--outline button--secondary button--lg"
-            to="https://forms.gle/NM17sCK82Wn75K337">
+            to="https://dsc.gg/skylink">
             Join our Discord 🎮
           </Link>
         </div>
