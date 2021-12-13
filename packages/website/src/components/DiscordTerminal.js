d594947b448ad2d150e598742917b3a6bfb63058
tisuela <ntisuela@uci.edu>
2021-12-12 19:12:10 -0800

    implement discord terminal

---
+++ packages/website/src/components/DiscordTerminal.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { InView } from "react-intersection-observer";
+import './discord-termynal.css';
+import Termynal from './termynal';
+
+export default class Terminal extends React.Component {
+
+   discordTerminal = null;
+   startedAlready = false;
+
+    componentDidMount() {
+        this.discordTerminal = new Termynal(this.t, {
+            typeDelay: 40,
+            lineDelay: 700,
+            cursor: '|',
+            prefix: 'disco',
+            terminalName: 'discord',
+        });
+    }
+
+    restartDiscordTerminal(discordTerminal, inView) {
+       
+       if (inView && discordTerminal != null && !this.startedAlready) {
+           discordTerminal.start();
+           this.startedAlready = true;
+       }
+       
+       
+    }
+
+    render() {
+        return (
+           <InView as="div" onChange={
+               (inView, entry, observer) => {
+                   this.restartDiscordTerminal(this.discordTerminal, inView)
+               }
+           }>
+                <div data-termynal style={this.props.style} ref={t => this.t = t}>
+                    {this.props.children}
+                </div>
+           </InView>
+        );
+    }
+}
